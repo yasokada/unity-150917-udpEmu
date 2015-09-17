4d73732091768e9961fb877d41afd0cf635b94fd
y_okada <yasokada@gmail.com>
2015-09-18 06:53:55 +0900

    adding MyCommandEmuUtil

---
+++ Assets/MyCommandEmuUtil.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic; // for Dictionary / List
+
+namespace NS_MyCommandEmuUtil
+{
+	public static class MyCommandEmuUtil {
+		static Dictionary <string, List<string>> myDic = null;
+
+		public static void Init() {
+			if (myDic == null) {
+				myDic = new Dictionary<string, List<string>> ();
+			}
+		}
+
+		public static void Add(string command, string response) {
+
+		}
+	}
+}
+
